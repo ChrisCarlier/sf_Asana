42366cc48442150b9c0f0cab804b1157eda40f3e
Chris <cj.carlier@gmail.com>
2020-01-23 23:40:29 +0100

    Mailer : * Envoi mail * Style * Image par lien ou embed * Correction style > appliquer le style à toutes les    balises automatiquement

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,17 @@ use App\Entity\ProjectSearch;
 use App\Entity\Team;
 use App\Form\ProjectSearchType;
 use phpDocumentor\Reflection\Types\Array_;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportException;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\NamedAddress;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
@@ -40,6 +47,78 @@ class HomeController extends AbstractController
     public function index() : Response
     {
         return $this->render('pages/home.html.twig');
+    }
+
+    /**
+     * @Route("/accessRequest", name="newAccessRequest")
+     * @param Request $request
+     * @param MailerInterface $mailer
+     * @return Response
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+     */
+    public function newAccessRequest(Request $request, MailerInterface $mailer)
+    {
+        $form = $this->createFormBuilder()
+            ->add('username',TextType::class)
+            ->add('email',TextType::class)
+            ->setMethod('POST')
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($request->isMethod('POST')) {
+            if ($form->isSubmitted() && $form->isValid()) {
+                $data = $form->getData();
+                $this->sendAccessRequest($data['username'],$data['email'],$mailer);
+                return $this->render('pages/message.html.twig',[
+                    'message' => 'Demande envoyée !'
+                ]);
+            }
+        }
+        return $this->render('security/accessRequest.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/msg", name="sendAccessRequest")
+     * @param MailerInterface $mailer
+     * @param $username
+     * @param $email
+     * @return Response
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
+     */
+    public function sendAccessRequest($username,$email,MailerInterface $mailer)
+    {
+        $email = (new TemplatedEmail())
+            ->from(new NamedAddress('no-reply@ccarlier.be','My BIV'))
+            ->to(new NamedAddress($email,$username))
+            ->subject('Demande d\'accès au site')
+            ->htmlTemplate('email\accessRequest.html.twig')
+            ->context([
+                'user'=> $username,
+                'email'=>$email
+            ]);
+        try{
+            $mailer->send($email);
+        }
+        catch (TransportException $e)
+        {
+            return $this->render('bundles/TwigBundle/Exception/error.html.twig');
+        }
+
+        return $this->render('pages/message.html.twig',[
+            'message' => 'Demande envoyée !'
+        ]);
+    }
+
+    /**
+     * @return Response
+     * @Route("/emailAccessRequest", name="emailAccessRequest")
+     */
+    public function viewEmailAccessRequest()
+    {
+        return $this->render('email/accessRequest.html.twig');
     }
 
 
